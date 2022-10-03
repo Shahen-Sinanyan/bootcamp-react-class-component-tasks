82bf2d6434e4a3c8d4e488253a2ed82fa2c69f0d
Shahen-Sinanyan <sinanyanshahen@gmail.com>
2022-10-03 16:41:37 +0400

    counter app added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import { Component } from 'react'
 import { render } from '@testing-library/react';
 
 // import Cardlist from './components/unmountingCardTracker/cardList';
-// import MainAppCounter from './components/counterApp/mainCounterApp';
+ import MainAppCounter from './components/counterApp/mainCounterApp';
 // import InputSearch from './components/searchDropdown/searchInput/index';
 
 class App extends Component{
 
   render() {
-    return(<></>
+    return(
       //<Cardlist/>
-     // <MainAppCounter/>
+     <MainAppCounter/>
       //<InputSearch/>
       
     )
